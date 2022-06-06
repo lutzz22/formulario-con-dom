8067120bc3b1da68f21acd3a30729e774aeb5ab6
luaramb <arambarrilucia@gmail.com>
2022-06-06 19:35:50 -0300

    ej17 form

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Escribe tu codigo JS acá
+
+let modal = document.querySelector('.modal')
+
+const envioFormulario=(event)=> {
+    event.preventDefault();
+    const nombreApellido=document.querySelector('#floatingName').value
+    console.log(nombreApellido)
+    const email=document.querySelector('#floatingInput').value
+    console.log(email)
+    const comentario=document.querySelector('#floatingTextarea').value
+    console.log(comentario)
+
+    modal.classList.toggle('modal-header')
+
+    const infoNombre=document.querySelector('#data-nombre')
+    infoNombre.textContent=nombreApellido;
+    const infoEmail=document.querySelector('#data-email')
+    infoEmail.textContent=email;
+    const infoComent=document.querySelector('#data-mensaje')
+    infoComent.textContent=comentario;
+}
+
+const cierreFormulario=()=>{
+    modal.classList.toggle('modal-header')
+}
